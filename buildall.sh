a8c225bb5d90d1c4e13ffd8c5cd937d3184c6b70
Peter Jakobs <pjakobs@mobileme.de>
2024-03-25 21:54:50 +0100

    trying to get it to work

---
+++ buildall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cpus=$(nproc)
+threads=$(( cpus * 2 ))
+export ESP_HOME=/opt/esp-quick-toolchain
+export SMING_HOME=/opt/Sming/Sming
+export IDF_PATH=IDF_PATH:=/opt/esp-idf
+export IDF_TOOLS_PATH=IDF_TOOLS_PATH:=/opt/esp32
+cd /home/pjakobs/esp_rgbww_build/esp_build/esp_rgbww_firmware
+git pull
+git checkout devel
+git submodule deinit Sming
+rm Sming -rf
+git rm Sming
+git submodule init
+git submodule sync
+git submodule update
+
+podman run --replace --name build_quasar -it -v /home/pjakobs/esp_rgbww_build/esp_build/:/build:z build_quasar /usr/bin/deploy
+podman run --replace --name build_sming -it -v /home/pjakobs/esp_rgbww_build/esp_build/:/build:z build_sming /usr/bin/make.firmware
--- setup.sh
+++ setup.sh
@@ -14,12 +14,29 @@ fi
 if [ ! -d $BUILDDIR/esp_rgb_webapp2 ]
 then 
 	cd $BUILDDIR
-	git clone git@github.com:pljakobs/esp_rgb_webapp2.git
+	#git clone git@github.com:pljakobs/esp_rgb_webapp2.git
+	git clone https://github.com/pljakobs/esp_rgb_webapp2.git
+fi
+if [ ! -d $BUILDDIR/esp_rgbww_firmware ]
+then
+	cd $BUILDDIR
+	#git clone git@github.com:pljakobs/esp_rgbww_firmware.git
+	git clone https://github.com/pljakobs/esp_rgbww_firmware.git
+	cd $BUILDDIR/esp_rgbww_firmware
+	git submodule deinit Sming
+	rm Sming -rf
+	git rm Sming
+	git stash
+	if [ ! -d spiffs ]
+	then
+		mkdir spiffs
+	fi
+	cd $installRoot
 fi
 cd $installRoot
 podman build -f containerfile.base -t build_base
 podman build -f containerfile.quasar -t build_quasar
-podman build -f containerfile.sming -t build_sming -v $BUILDDIR/:/build:z 
+podman build -f containerfile.sming -t build_sming 
 
 cat <<EOF >buildall.sh
 #!/bin/bash
@@ -32,10 +49,12 @@ export IDF_TOOLS_PATH=IDF_TOOLS_PATH:=/opt/esp32
 cd $BUILDDIR/esp_rgbww_firmware
 git pull
 git checkout devel
+git submodule deinit Sming
+rm Sming -rf
+git rm Sming
 git submodule init
 git submodule sync
 git submodule update
-make -j \$threads SMING_SOC=esp8266
 
 podman run --replace --name build_quasar -it -v $BUILDDIR/:/build:z build_quasar /usr/bin/deploy
 podman run --replace --name build_sming -it -v $BUILDDIR/:/build:z build_sming /usr/bin/make.firmware
