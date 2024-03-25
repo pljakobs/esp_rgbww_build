217ff80dd0e59eb086fda3a4ed5040f62130358f
Peter Jakobs <pjakobs@mobileme.de>
2024-03-25 19:06:38 +0100

    fixes to the sming build container

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 installRoot=$(pwd)
 if [ "$1"=="" ]
 then
-	BUILDDIR=$pwd/esp_build
+	BUILDDIR=$installRoot/esp_build
 else
 	BUILDDIR=$1
 fi
 
 if [ ! -d $BUILDDIR ]
 then
-	mkdir $BUILDDIR
+	mkdir -p $BUILDDIR
 	cd $BUILDDIR
 fi
 if [ ! -d $BUILDDIR/esp_rgb_webapp2 ]
@@ -16,31 +16,28 @@ then
 	cd $BUILDDIR
 	git clone git@github.com:pljakobs/esp_rgb_webapp2.git
 fi
-if [ ! -d $BUILDDIR/esp_rgbww_firmware ]
-then
-	git clone git@github.com:pljakobs/esp_rgbww_firmware
-fi
-cd $installRoot
 podman build -f containerfile.base -t build_base
 podman build -f containerfile.quasar -t build_quasar
-podman build -f containerfile.sming -t build_sming
-echo <<EOF >buildall.sh
+podman build -f containerfile.sming -t build_sming -v $BUILDDIR/:/build:z 
+
+cat <<EOF >buildall.sh
 #!/bin/bash
-cpus=$(nproc)
-threads=$(( cpus * 2 ))
+cpus=\$(nproc)
+threads=\$(( cpus * 2 ))
 export ESP_HOME=/opt/esp-quick-toolchain
 export SMING_HOME=/opt/Sming/Sming
-export IDF_PATH=${IDF_PATH:=/opt/esp-idf}
-export IDF_TOOLS_PATH=${IDF_TOOLS_PATH:=/opt/esp32}
+export IDF_PATH=IDF_PATH:=/opt/esp-idf
+export IDF_TOOLS_PATH=IDF_TOOLS_PATH:=/opt/esp32
 cd $BUILDDIR/esp_rgbww_firmware
 git pull
 git checkout devel
 git submodule init
 git submodule sync
 git submodule update
-make -j $threads SMING_SOC=esp8266
-[pjakobs@buildhost esp_rgbww_build]$ cat buildall.sh
+make -j \$threads SMING_SOC=esp8266
+
 podman run --replace --name build_quasar -it -v $BUILDDIR/:/build:z build_quasar /usr/bin/deploy
-podman run --replace --name build_sming -it -v $BUILDDIR:/build:z build_sming /usr/bin/make.firmware
+podman run --replace --name build_sming -it -v $BUILDDIR/:/build:z build_sming /usr/bin/make.firmware
 EOF
+
 chmod +x buildall.sh
