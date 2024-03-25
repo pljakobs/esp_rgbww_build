091bbafde45bc29a081dd33578aafd148a69f933
Peter Jakobs <pjakobs@mobileme.de>
2024-03-25 09:12:10 +0100

    integrated creating the build directory and script in the setup

--- buildall.sh
+++
@@ -1,3 +0,0 @@
-podman run --replace --name build_quasar -it -v /home/pjakobs/nginx/html/:/html:z -v /home/pjakobs/build/:/build:z build_quasar /usr/bin/deploy
-podman run --replace --name build_sming -it -v /home/pjakobs/nginx/html/:/html:z -v /home/pjakobs/build/:/build:z build_sming /usr/bin/make.firmware
-
