1629156720f4a359abfbf06a3e60cb4ce7323244
buildhost <pjakobs@mobileme.de>
2024-03-25 07:21:05 +0000

    added cloning the repos first

--- setup.sh
+++ setup.sh
@@ -1,3 +1,19 @@
+installRoot=$(pwd)
+if [ ! -d ~/build ]
+then
+	mkdir ~/build
+	cd ~/build
+fi
+if [ ! -d ~/build/esp_rgb_webapp2 ]
+then 
+	cd ~/build
+	git clone git@github.com:pljakobs/esp_rgb_webapp2.git
+fi
+if [ ! -d ~/build/esp_rgbww_firmware ]
+then
+	git clone git@github.com:pljakobs/esp_rgbww_firmware
+fi
+cd $installRoot
 podman build -f containerfile.base -t build_base
 podman build -f containerfile.quasar -t build_quasar
 podman build -f containerfile.sming -t build_sming
